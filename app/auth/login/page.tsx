5d84f0a4e5e8d590660beb43cf4808fdc2b7b735
Daniel Batista <dani_batista20@hotmail.com>
2024-04-19 04:49:41 +0100

    add: HomePage & LoginButton

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <main
+      className="flex h-full flex-col items-center justify-center
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-400 to-cyan-900
+    "
+    >
+      Login Page
+    </main>
+  )
+}
+
+export default Login
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+// 'use client'
+import { cn } from '@/lib/utils'
+import { Poppins } from 'next/font/google'
+import { LoginButton } from '@/components/ui/auth/login-button'
+import { Button } from '@/components/ui/button'
+
+const poppins = Poppins({ subsets: ['latin'], weight: ['600'] })
+
 export default function Home() {
-  return <h1>home</h1>
+  return (
+    <main
+      className="flex h-full flex-col items-center justify-center
+      bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-cyan-400 to-cyan-900
+    "
+    >
+      <section className="space-y-6 text-center">
+        <h1
+          className={cn(
+            'text-6xl font-bold text-cyan-100 drop-shadow-md',
+            poppins.className
+          )}
+        >
+          🔓 Auth
+        </h1>
+        <p className="font-semibold text-lg">A Simple Authentication service</p>
+
+        <LoginButton asChild>
+          <Button
+            variant={'secondary'}
+            size={'lg'}
+            // onClick={() => {
+            //   console.log('sign in')
+            // }}
+          >
+            Sign in
+          </Button>
+        </LoginButton>
+      </section>
+    </main>
+  )
 }
