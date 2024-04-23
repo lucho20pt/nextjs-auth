e8378b225157cd21b012e19dd2b2a5b658f0a0d2
Daniel Batista <dani_batista20@hotmail.com>
2024-04-23 16:09:00 +0100

    add: Auth - CardWrapper

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,16 +2,7 @@ import { Loginform } from '@/components/auth/login-form'
 import { CardWrapper } from '@/components/auth/card-wrapper'
 
 const LoginPage = () => {
-  return (
-    <CardWrapper
-      headerLabel="Wellcome Back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
-      showSocial
-    >
-      <Loginform />
-    </CardWrapper>
-  )
+  return <Loginform />
 }
 
 export default LoginPage
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { CardWrapper } from './card-wrapper'
 
 export const Loginform = () => {
-  return <section>Loginform</section>
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      Login form
+    </CardWrapper>
+  )
 }
