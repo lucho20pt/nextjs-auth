726127993be77a8cf55ac76eae9ae5d45c523df0
Daniel Batista <dani_batista20@hotmail.com>
2024-06-03 23:42:37 +0100

    add: AuthConfig - Credentials, authorize, validatedFields, getUserByEmail()

--- auth.config.ts
+++ auth.config.ts
@@ -4,9 +4,29 @@
  * You can put all common configuration here which does not rely on the adapter.
  * Notice this is exporting a configuration object only, we’re not calling NextAuth() here.
  */
-
 import type { NextAuthConfig } from 'next-auth'
-import GitHub from 'next-auth/providers/github'
-import Google from 'next-auth/providers/google'
+import Credentials from 'next-auth/providers/credentials'
+import bcryptjs from 'bcryptjs'
 
-export default { providers: [GitHub, Google] } satisfies NextAuthConfig
+import { LoginSchema } from '@/schema'
+import { getUserByEmail } from './data/user'
+
+export default {
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials)
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data
+
+          const user = await getUserByEmail(email)
+          if (!user || !user.password) return null
+
+          const passwordMatch = await bcryptjs.compare(user.password, password)
+          if (passwordMatch) return user
+        }
+        return null
+      },
+    }),
+  ],
+} satisfies NextAuthConfig
