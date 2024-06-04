0848d7482c5b8adee27e5c41d4c4c5011d2ecc50
Daniel Batista <dani_batista20@hotmail.com>
2024-06-05 00:23:57 +0100

    add: auth - authConfig & signIn & signOut

--- auth.ts
+++ auth.ts
@@ -6,14 +6,16 @@
  */
 
 import NextAuth from 'next-auth'
+import authConfig from '@/auth.config'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 
 import { db } from '@/lib/db'
-import authConfig from '@/auth.config'
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut
 } = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' }, // prisma cannot use database strategy, cause don't work on the edge
