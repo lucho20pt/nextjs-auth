a0cfc856a39e07770f6f246b941db0963eb7ddac
Daniel Batista <dani_batista20@hotmail.com>
2024-06-05 11:44:30 +0100

    add: actions/login - signIn('credentials')

--- actions/login.ts
+++ actions/login.ts
@@ -1,7 +1,10 @@
 'use server'
-
 import { z } from 'zod'
+import { AuthError } from 'next-auth'
+import { signIn } from '@/auth'
+
 import { LoginSchema } from '@/schema'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values)
@@ -10,5 +13,27 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: 'Invalid fields!' }
   }
 
-  return { success: 'Login Sent' }
+  const { email, password } = validatedFields.data
+
+  try {
+    const resultSignIn = await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+
+    if (!resultSignIn) {
+      throw new Error('Sign in failed')
+    }
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return { error: 'Invalid Credentials!' }
+        default:
+          return { error: 'Something went wrong!' }
+      }
+    }
+    throw error
+  }
 }
