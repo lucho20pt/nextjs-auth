71a7ed8ece986060b9b2cd9ac83e8931b7393eae
Daniel Batista <dani_batista20@hotmail.com>
2024-06-05 23:24:48 +0100

    add: settings - logout button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,23 @@
-import { auth } from '@/auth'
+import { auth, signOut } from '@/auth'
 
 const SettingsPage = async () => {
   const session = await auth()
   return (
-    <div className="text-2xl text-center">
-      Settings Page {JSON.stringify(session)}
+    <div className="text-md">
+      <h4>Settings Page</h4>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          'use server'
+          await signOut({ redirectTo: '/auth/login' })
+        }}
+      >
+        <div className="text-center">
+          <button className="btn border p-2 bg-gray-700 hover:bg-gray-400 text-white rounded-md">
+            Sign Out
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
