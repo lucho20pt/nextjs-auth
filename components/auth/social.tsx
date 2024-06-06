a8e43db4d05c3336085eec802c37a46de7e87f6d
Daniel Batista <dani_batista20@hotmail.com>
2024-06-06 01:57:58 +0100

    add: social login - onClick() Google & Github

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,19 +1,34 @@
 'use client'
-
+import { signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaGithub } from 'react-icons/fa'
 import { Button } from '@/components/ui/button'
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes'
 
-interface SocialProps {}
+export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    })
+  }
 
-export const Social = ({}: SocialProps) => {
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button className='w-full' size={'lg'} variant={'outline'} onClick={() => {}}>
+      <Button
+        onClick={() => onClick('google')}
+        className="w-full"
+        size={'lg'}
+        variant={'outline'}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
 
-      <Button className='w-full' size={'lg'} variant={'outline'} onClick={() => {}}>
+      <Button
+        onClick={() => onClick('github')}
+        className="w-full"
+        size={'lg'}
+        variant={'outline'}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
